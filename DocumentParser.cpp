61d9b166924bf02bab07a93c2339d245abcf811e
riamuk101 <rmukherji@smu.edu>
2023-11-03 19:27:37 -0500

    Parser can do Titles and Author!

--- DocumentParser.cpp
+++ DocumentParser.cpp
@@ -5,12 +5,59 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <iomanip>
 
+using namespace rapidjson;
+using namespace std;
 
-
-void DocumentParser::buildIndex(const std::string& document) {
-    std::ifstream file(document);
-    if (!file.good()) {
-        throw std::invalid_argument("File could not be opened: " + document);
+void DocumentParser::parseDocument(const string& jsonContent) {
+    ifstream ifs(jsonContent);
+    if (!ifs.is_open()) {
+        cerr << "Could not open file for reading: " << jsonContent << endl;
+        return;
     }
+
+    IStreamWrapper isw(ifs);
+    Document d;
+    d.ParseStream(isw);
+
+    if (d.HasParseError()) {
+        cerr << "JSON parse error: " << d.GetParseError() << endl;
+        return;
+    }
+
+   d.ParseStream(isw);
+
+    // Now that the document is parsed, we can access different elements the JSON using
+    // familiar subscript notation.
+
+    // This accesses the -title- element in the JSON. Since the value
+    //  associated with title is a string (rather than
+    //  an array or something else), we call the GetString()
+    //  function to return the actual title of the article
+    //  as a c-string.
+    auto val = d["title"].GetString();
+    cout << "Title: " << val << "\n";
+
+    // The Persons entity for which you're building a specific
+    //  inverted index is contained in top level -entities- element.
+    //  So that's why we subscript with ["entities"]["persons"].
+    //  The value associated with entities>persons is an array.
+    //  So we call GetArray() to get an iterable collection of elements
+    auto persons = d["entities"]["persons"].GetArray();
+
+    // We iterate over the Array returned from the line above.
+    //  Each element kind of operates like a little JSON document
+    //  object in that you can use the same subscript notation
+    //  to access particular values.
+    cout << "  Person Entities + sentiment:"
+         << "\n";
+    for (auto &p : persons)
+    {
+        cout << "    > " << setw(30) << left << p["name"].GetString()
+             << setw(10) << left << p["sentiment"].GetString() << "\n";
+    }
+
+    cout << endl;
 }
+
--- DocumentParser.h
+++ DocumentParser.h
@@ -5,17 +5,24 @@
 #include <string>
 #include <vector>
 #include <map>
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/document.h"
+
 
 class DocumentParser
 {
 private:
 
+
 //Might need to change this but currently declared as a nested map
 //the first part is a string which represents the keyword we are looking for in a document or query - For example if we were trying to find the word apple and all related documents the string would be apple
 //the second part is the nested map, but basically the key is an int. The int represents the ID of the document since there are many documents. For example if it was document 100 the key would be 100
 // The vector of ints should represent the location of where the matching words are in the specific document. If the word apple is mentioned in the first 5th and 7th word it would store a vector of {1,5,7}
-std::map<std::string, std::map<int, std::vector<int>>> index;
+///brief param return 
+//std::map<std::string, std::map<int, std::vector<int>>> index;
 public:
-void buildIndex(const std::string& document);
+//void buildIndex(const std::string& document);
+
+void parseDocument(const std::string& jsonContent);
 };
 #endif
