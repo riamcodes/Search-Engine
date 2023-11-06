c1ec6cba4e9638c17e3297fe549300ae77edd41c
Grace Linda Schultz <gschultz@genuse54.engr.smu.edu>
2023-11-06 14:46:31 -0600

    query processor and ui updates

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -1,8 +1,8 @@
 #include "UserInterface.h"
 
-void UserInterface::printInitialQuestion()
+void UserInterface::initialQuestion()
 {
-    while (!(answer1 == "1" || answer1 == "2" || answer1 == "3"))
+    while (!(answer == "1" || answer == "2" || answer == "3"))
     {
         std::cout << "Hello! Welcome to our search engine" << std::endl;
         std::cout << "Please enter a number 1-3 of the choices below to continue" << std::endl;
@@ -10,13 +10,30 @@ void UserInterface::printInitialQuestion()
         std::cout << "1) Create an index from a directory with documents" << std::endl;
         std::cout << "2) Write and read the index to a file (make it persistant)" << std::endl;
         std::cout << "3) Enter a query" << std::endl;
-        std::cin >> answer1;
-        if (!(answer1 == "1" || answer1 == "2" || answer1 == "3"))
+        getline(answer);
+        if (!(answer == "1" || answer == "2" || answer == "3"))
         {
             std::cout << "Error! This is an invalid answer. Please select numbers 1 through 3." << std::endl;
         }
     }
-    //processingQuery(answer1);
+}
+void UserInterface::secondQuestion() {
+    if (answer == "1")
+    {
+        std::cout << "Please enter an index to create." << std::endl;
+        getline(answer1);
+    }
+    else if (answer == "2")
+    {
+        std::cout << "Something that makes sense for this." << std::endl;
+        getline(answer2);
+    }
+    else if (answer == "3")
+    {
+        std::cout << "Please enter a query" << std::endl;
+        getline(answer3);
+        parsingAnswer(answer3);
+    }
 }
 
 // void UserInterface::directAnswer()
