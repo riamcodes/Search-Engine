d09a0cab3ee45f6c63c5802dc7e0262585ce47c2
riamuk101 <rmukherji@smu.edu>
2023-11-23 18:55:55 -0600

    Doc parser completed

--- DocumentParser.cpp
+++ DocumentParser.cpp
@@ -9,13 +9,33 @@
 #include <filesystem>
 #include <dirent.h>
 #include <algorithm>
+#include "IndexHandler.h"
+#include <set>
 #include "porter2_stemmer.h" // Include the Porter Stemmer header
 //str tok in the remove  or use the existing sentiment analyzer
 using namespace rapidjson;
 using namespace std;
 
+//list of stopwords found from https://gist.github.com/sebleier/554280 NLTK list of english stopwords
+set<string> stopWords = { "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your",
+    "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her",
+    "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs",
+    "themselves", "what", "which", "who", "whom", "this", "that", "these", "those",
+    "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had",
+    "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or",
+    "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against",
+    "between", "into", "through", "during", "before", "after", "above", "below", "to", "from",
+    "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once",
+    "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more",
+    "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too",
+    "very", "s", "t", "can", "will", "just", "don", "should", "now"};
+
 void DocumentParser::parseDocument(const string& jsonContent) {
+    IndexHandler index;
+    int wordCount = 0;
     string docID;
+    string docPersons;
+    string org;
     ifstream ifs(jsonContent);
     if (!ifs.is_open()) {
         cerr << "Could not open file for reading: " << jsonContent << endl;
@@ -34,37 +54,19 @@ void DocumentParser::parseDocument(const string& jsonContent) {
    d.ParseStream(isw);
   
 
-    // // Now that the document is parsed, we can access different elements the JSON using
-    // // familiar subscript notation.
-
-    // // This accesses the -title- element in the JSON. Since the value
-    // //  associated with title is a string (rather than
-    // //  an array or something else), we call the GetString()
-    // //  function to return the actual title of the article
-    // //  as a c-string.
-    // auto val = d["title"].GetString();
-    // cout << "Title: " << val << "\n";
-
-    // // The Persons entity for which you're building a specific
-    // //  inverted index is contained in top level -entities- element.
-    // //  So that's why we subscript with ["entities"]["persons"].
-    // //  The value associated with entities>persons is an array.
-    // //  So we call GetArray() to get an iterable collection of elements
-    // auto persons = d["entities"]["persons"].GetArray();
-
-    // // We iterate over the Array returned from the line above.
-    // //  Each element kind of operates like a little JSON document
-    // //  object in that you can use the same subscript notation
-    // //  to access particular values.
-    // cout << "  Person Entities + sentiment:"
-    //      << "\n";
-    // for (auto &p : persons)
-    // {
-    //     cout << "    > " << setw(30) << left << p["name"].GetString()
-    //          << setw(10) << left << p["sentiment"].GetString() << "\n";
-    // }
+   
 if (d.HasMember("uuid") && d["uuid"].IsString()) {
      docID = d["uuid"].GetString();
+  //   index.addDocument(int, document); ask anekah how this works 
+}
+
+if (d.HasMember("persons") && d["persons"].IsString()) {
+     docPersons = d["persons"].GetString();
+  //   index.addPeople(std::string, int) ask anekah how this works 
+}
+if (d.HasMember("organizations") && d["organizations"].IsString()) {
+     org = d["organizations"].GetString();
+  //   void addOrgs(int, document); ask anekah how this works WARNING THIS IS USUALLY BLANK
 }
 
 
@@ -81,14 +83,20 @@ if (d.HasMember("uuid") && d["uuid"].IsString()) {
             //processes word from beginning to end and then goes back to the beginning and makes everyting lowercase 
             transform(word.begin(), word.end(), word.begin(), ::tolower); 
             Porter2Stemmer::stem(word);
-            cout << docID<< endl; 
+            //cout << docID<< endl; 
+             if (stopWords.find(word) == stopWords.end()) {
             cout << word << endl;  // Print each word on a new line
+            wordCount++;
+          //  index.addWords(word, docID); ASK ANEKAH HOW THIS WORKS 
+
+             }
         }
     } else {
         cerr << "The JSON does not contain a 'text' attribute or it is not a string." << endl;
     }
 
     cout << endl;
+    cout << "Document ID: " << docID << " Word Count: " << wordCount++ << endl;
 }
 
 // // make a directroy set it to to the big file then dirent read directory of the big file  as long as it doesnt equal nullptr
@@ -103,6 +111,7 @@ if (d.HasMember("uuid") && d["uuid"].IsString()) {
 
 
 void DocumentParser::traverseSubdirectory(const string& directoryPath){
+ 
 // Open the directory
     DIR* dir = opendir(directoryPath.c_str()); //c_str so it can be passed to opendir
     if (dir == nullptr) {
