3d8e90ac6b5abf39a1eccf29767aba7eb5c12331
Anekah Kelley <askelley@smu.edu>
2023-11-28 22:14:01 -0600

    cleaned up ih

--- IndexHandler.h
+++ IndexHandler.h
@@ -9,44 +9,31 @@
 #include "DSDocument.h"
 #include <algorithm>
 
-// This class should contain a lot of setter/getter functions for the avl and hash index
-// When we want to add or get a query then we will call these functions
-// We also need to store all of the documents
-// Two ways to do this//
-// These two options also depend on the document parser, so communicate on this
-// 1.) Store all needed data in a vector for quick and easy access (def an easier option to code)(internal storage)
-// 2.) Store file path to save storage (external storage)
-// This class needs to implement persistence (very difficult but recommended to save until end)
-// Should also store the statistics (timing, word count, etc) -> (see instructions)
-// Print function is implemented in the AVLTree (printTree())
 class IndexHandler
 {
 private:
-//int tf
-//int idf
-DSAvlTree<std::string, std::string> words;
-Hash<std::string, std::string> people;
-Hash<std::string, std::string> orgs;
-std::map<std::string, std::string> docs;
-std::map<std::string, int> wordCount;
+    DSAvlTree<std::string, std::string> words;
+    Hash<std::string, std::string> people;
+    Hash<std::string, std::string> orgs;
+    std::map<std::string, std::string> docs;
+    std::map<std::string, int> wordCount;
 
 public:
-std::map<std::string, int> getWords(std::string);
-std::map<std::string, int> getPeople(std::string);
-std::map<std::string, int> getOrgs(std::string);
-std::string getFilePath(std::string);
-std::string getTitle(std::string);
+    std::map<std::string, int> getWords(std::string);
+    std::map<std::string, int> getPeople(std::string);
+    std::map<std::string, int> getOrgs(std::string);
+    std::string getFilePath(std::string);
+    std::string getTitle(std::string);
 
-int getWordCount(std::string);
-void addWords(std::string, std::string);
-void addPeople(std::string, std::string);
-void addOrgs(std::string, std::string);
-void addDocument(std::string, std::string);
-void addWordCount(std::string, int);
-int getDocSize();
-void createPersistence();
-void readPersistence();
-int returnNumArticles(std::string, std::string);
-int returnSize(std::string);
+    int getWordCount(std::string);
+    void addWords(std::string, std::string);
+    void addPeople(std::string, std::string);
+    void addOrgs(std::string, std::string);
+    void addDocument(std::string, std::string);
+    void addWordCount(std::string, int);
+    int getDocSize();
+    void createPersistence();
+    void readPersistence();
+    int returnSize();
 };
 #endif
