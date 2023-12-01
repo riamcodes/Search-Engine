78bee6e9ae0e5c122950d9d3bb2b0d4efa246bbe
Grace Linda Schultz <gschultz@genuse54.engr.smu.edu>
2023-11-30 21:12:48 -0600

    fake testing

--- test_QueryProcessor.cpp
+++ test_QueryProcessor.cpp
@@ -1,21 +1,23 @@
 #include "test_QueryProcessor.h"
 
-void test_QueryProcessor::test()
-{
-    IndexHandler* ih;
-    QueryProcessor* qp;
-    qp->setIndexHandler(&ih);
 
-    // addDocument() needs the filepath and the title
-    ih->addDocument("../sample_data/coll_1/news_0064567.json", "German firms doing business in UK gloomy about Brexit - survey");
-    ih->addDocument("../sample_data/coll_1/news_0064569.json", "European stocks open lower on Powell’s comments; Solvay, Carrefour to report");
-    ih->addDocument("../sample_data/coll_2/news_0064571.json", "BRIEF-Kewaunee Scientific Q3 Earnings Per Share $0.31");
 
-    std::string query = "";
-    std::vector<std::string> result = qp->parsingAnswer(query);
+// void test_QueryProcessor::test()
+// {
+//     IndexHandler* ih;
+//     QueryProcessor* qp;
+//     qp->setIndexHandler(&ih);
 
-    for (const auto &document : result)
-    {
-        std::cout << document << std::endl;
-    }
-}
+//     // addDocument() needs the filepath and the title
+//     ih->addDocument("../sample_data/coll_1/news_0064567.json", "German firms doing business in UK gloomy about Brexit - survey");
+//     ih->addDocument("../sample_data/coll_1/news_0064569.json", "European stocks open lower on Powell’s comments; Solvay, Carrefour to report");
+//     ih->addDocument("../sample_data/coll_2/news_0064571.json", "BRIEF-Kewaunee Scientific Q3 Earnings Per Share $0.31");
+
+//     std::string query = "";
+//     std::vector<std::string> result = qp->parsingAnswer(query);
+
+//     for (const auto &document : result)
+//     {
+//         std::cout << document << std::endl;
+//     }
+// }
