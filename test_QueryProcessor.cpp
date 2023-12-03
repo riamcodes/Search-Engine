03fbf976212970ddcfaaf27ab8705aacd7e820f6
Grace Linda Schultz <gschultz@genuse54.engr.smu.edu>
2023-12-03 01:17:55 -0600

    ahhh

--- test_QueryProcessor.cpp
+++ test_QueryProcessor.cpp
@@ -10,70 +10,22 @@
 
 TEST_CASE("query processor", "[QueryProcessor.h]")
 {
-    IndexHandler indexObject;
-    DocumentParser documentObject;
-   // documentObject.setIndex(indexObject);
-    //documentObject.();//
-    QueryProcessor queryObject;
-    queryObject.setIndexHandler(indexObject);
-    std::map<std::string, int> results;
-    results = queryObject.parsingAnswer("ago");
-    REQUIRE(results.size() == 1);
-    //REQUIRE(results[0] == "");
-    results = queryObject.parsingAnswer("common ORG:reuters");
-    REQUIRE(results.size() == 1);
-    //REQUIRE(results[0] == "");
-    results = queryObject.parsingAnswer("common PERSON:schweitzer");
-    REQUIRE(results.size() == 1);
-    //REQUIRE(results[0] == "");
-    results = queryObject.parsingAnswer("compani - compar");
-    REQUIRE(results.size() == 2);
-    //REQUIRE(results[0] == "");
-    //REQUIRE(results[0] == "");
-    results = queryObject.parsingAnswer("hahalolol");
-    REQUIRE(results.size() == 0);
+    DocumentParser dp;
+    IndexHandler ih;
+    QueryProcessor qp;
 
+    dp.parseDocument("/users7/cse/gschultz/assignment-4-search-engine-exit-code-0/sample_data/coll_1/news_0064567.json");
+
+    ih = dp.getIndex();
+    ih.createPersistence();
+    ih.readPersistence();
+    ih = dp.getIndex();
+    qp.setIndexHandler(ih);
+
+    std::map<std::string, int> relevantDocs = qp.parsingAnswer("common PERSON:schweitzer");
+
+    REQUIRE(relevantDocs.size() == 1);
+
+    std::cout << "Is printVector empty 1 means yes 0 means no " << std::endl;
+    std::cout << qp.printVector.empty();
 }
-    // std::cout << "hello";
-//     // IndexHandler ih;
-//     QueryProcessor qp;
-//     DocumentParser dp;
-
-//     dp.parseDocument("./sample_data");
-//     // dp.parseDocument("../sample_data/coll_1/news_0064569.json");
-//     // dp.parseDocument("../sample_data/coll_2/news_0064571.json");
-//     IndexHandler ih = dp.getIndex();
-//     qp.setIndexHandler(&ih);
-//     std::cout << "made it to here" << std::endl;
-
-// //     // addDocument() needs the filepath and the title
-// //     ih->addDocument("../sample_data/coll_1/news_0064567.json", "German firms doing business in UK gloomy about Brexit - survey");
-// //     ih->addDocument("../sample_data/coll_1/news_0064569.json", "European stocks open lower on Powell’s comments; Solvay, Carrefour to report");
-// //     ih->addDocument("../sample_data/coll_2/news_0064571.json", "BRIEF-Kewaunee Scientific Q3 Earnings Per Share $0.31");
-
-//     // std::cout << ih.getWordCount("German firms doing business in UK gloomy about Brexit - survey") << std::endl;
-//     // std::cout << ih.getWordCount("European stocks open lower on Powell’s comments; Solvay, Carrefour to report") << std::endl;
-//     // std::cout << ih.getWordCount("BRIEF-Kewaunee Scientific Q3 Earnings Per Share $0.31") << std::endl;
-
-//   std::string query1 = "prospects PERSON:eric ORG:bdi";
-//   qp.parsingAnswer(query1);
-//   std::string query2 = "JANUARY PERSON:strax PERSON:ab";
-//   qp.parsingAnswer(query2);
-//   std::cout << "Contents of printVector:" << std::endl;
-//             for (const auto& item : qp.printVector) {
-//                 std::cout << item << std::endl;
-//             }
-//   //std::string query = "businesses hi hello";
-//   // Porter2Stemmer::stem(query1);
-//   // std::cout << query1 << std::endl;
-//   // qp.parsingAnswer(query1);
-
-
-//     // std::string query = "";
-//     // std::vector<std::string> result = qp->parsingAnswer(query);
-
-//     // for (const auto &document : result)
-//     // {
-//     //     std::cout << document << std::endl;
-//     // }
-//     return 0;
