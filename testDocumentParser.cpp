6d9014a2f7203e33de25e4392d3668c00081f0c3
Grace Linda Schultz <gschultz@genuse54.engr.smu.edu>
2023-12-04 00:56:41 -0600

    testDocParser

--- testDocumentParser.cpp
+++ testDocumentParser.cpp
@@ -1,39 +1,45 @@
-/**
- * Example code related to final project
- */
 #include <iostream>
 #include <filesystem>
-#include <fstream>
 #include <string>
 #include <iomanip>
-#include <filesystem>
 #include <dirent.h>
-
-// RapidJSON headers we need for our parsing.
 #include "rapidjson/istreamwrapper.h"
 #include "rapidjson/document.h"
 #include "DocumentParser.h"
 using namespace rapidjson;
-using namespace std;
 
-// // Function Prototypes
-// void testReadJsonFile(const string &fileName);
-// void testFileSystem(const string &path);
-
+//catch 2 tests for this are not feasible considering return types and complexity of functions
+//testing test_IndexHandler.cpp requires doc parser to be working
+//therefore cout tests 
 int main()
 {
-
-//cout << "hello world";
-
  DocumentParser parser;
- //parser.printDocument("/users7/cse/rmukherji/assignment-4-search-engine-exit-code-0/kaggleSample6000/2018_03_112b52537b67659ad3609a234388c50a/news_0022791.json");
-   //parser.parseDocument("/users7/cse/rmukherji/assignment-4-search-engine-exit-code-0/kaggleSample6000/2018_03_112b52537b67659ad3609a234388c50a/news_0022791.json");
-   parser.printInfo("/users7/cse/rmukherji/assignment-4-search-engine-exit-code-0/sample_data/coll_1/news_0064567.json");
+ 
+ //printInfo test
+ std::cout << "Testing printInfo function";
+ std::cout << "Expected print:" << std::endl;
+ std::cout << "Title: German firms doing business in UK gloomy about Brexit - survey, Publication: reuters.com, Date Published: 2018-02-27" << std::endl << std::endl;
+ std::cout << "Actual print:" << std::endl;
+ parser.printInfo("../sample_data/coll_1/news_0064567.json");
+ std::cout << std::endl << std::endl;
 
+ //parseDocument test
+ std::cout << "Testing parseDocument function";
+ std::cout << "Expected print:" << std::endl;
+ std::cout << "Document ID: ../sample_data/coll_1/news_0064567.json Word Count: 251" << std::endl << std::endl;
+ std::cout << "Actual print:";
+ parser.parseDocument("../sample_data/coll_1/news_0064567.json");
+ std::cout << std::endl << std::endl;
 
-//Uncomment the following lines to do every single document. The lines above this test docParser on only one json file to see whats going on/
-//string bigFilePath = "/users7/cse/rmukherji/assignment-4-search-engine-exit-code-0/kaggleSample6000"; // Replace with the path to your directory
-// parser.traverseSubdirectory(bigFilePath);//
-//
-////
+ //printDocument test
+ std::cout << "The following should print out the text from the json" << std::endl;
+ std::cout << "Starting with:" << std::endl;
+ std::cout << " Title: German firms doing business in UK gloomy about Brexit - survey" << std::endl;
+ std::cout << "Text: BERLIN (Reuters) - German businesses that trade in Britain are pessimistic about their prospects there, and some are planning to move" << std::endl;
+ std::cout << "Ending with:" <<std::endl;
+ std::cout << "Writing by Paul Carrel; Editing by Hugh Lawson" << std::endl;
+ std::cout << "Actual test" << std::endl;
+ parser.printDocument("../sample_data/coll_1/news_0064567.json");
+ std::cout << std::endl << std::endl;
+
 }
