dbcdf9fc185e31eeab2de71ddd168599112eb0ad
riamcodes <138901333+riamcodes@users.noreply.github.com>
2024-02-05 20:43:16 -0600

    Update main.cpp so it can run without argv
    arguments were a little confusing removed so it can be directly run

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "SearchEngine.h" //include the search engine
+#include "UserInterface.h"
 using namespace std;
 
-int main(int argc, char **argv)
+int main()//int argc, char **argv
 {
-    SearchEngine se;      // create an instances of search engine
-    se.input(argc, argv); // pass in argc and argv
+//    SearchEngine se;      // create an instances of search engine
+//    se.input(argc, argv); // pass in argc and argv
+UserInterface ui;
+ui.initialQuestion();
 }
